f4ce4bf3034c616b8d6204f27de27a5d4d639841
kirillram-dev <sergeyfedorov249@gmail.com>
2026-02-13 19:32:22 +0300

    нахождение простых чисел до тысячи

---
+++ lw1/primes/primes/primee.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+
+
+int main()
+{
+	// моя первая программа
+	
+	std::vector<size_t> prim;
+	for (size_t i = 0; i < 1000; i++)
+	{
+		prim.push_back(i);
+	}
+	
+	for (size_t i = 2; i < 1000; i++)
+	{
+		if (prim[i] != 0)
+		{
+			size_t d = prim[i];
+			for (size_t i = 2 * d; i < 100; i = i + d)
+			{
+
+				prim[i] = 0;
+			}
+		}
+	}
+	
+	for (size_t i = 2; i < 1000; i++)
+	{
+		if (prim[i] != 0)
+		{
+			std::cout << prim[i] << " ";
+		}
+	}
+
+	std::cout << std::endl;
+	return 0;
+} 
---
+++ lw1/primes/primes/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+	int num = 67;
+	num = 52;
+	std::cout << num << "six seven" << 67 << std::endl;
+
+	int a, b;
+	std::cin >> a;
+	std::cout << a;
+
+	short num1 = 1000000;
+
+	return 0;
+}
