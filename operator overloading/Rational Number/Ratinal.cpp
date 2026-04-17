9f43395c74995c5160aa1529321f0ba9b743388e
kirillram-dev <sergeyfedorov249@gmail.com>
2026-04-17 20:30:40 +0300

    добавлена операция сложения рац чисел

--- operator overloading/Rational Number/Ratinal.cpp
+++ operator overloading/Rational Number/Ratinal.cpp
@@ -1,4 +1,6 @@
 #include "Ratinal.h"
+#include <math.h>
+#include <numeric>
 
 	CRational::CRational()
 	: m_numerator(0)
@@ -29,4 +31,12 @@
 	double CRational::ToDouble() const
 	{
 		return static_cast<double>(m_numerator / m_denominator);
+	}
+
+	CRational const operator+(const CRational& lhs, const CRational& rhs)
+	{
+		int denominator = std::lcm(lhs.GetDenominator(), rhs.GetNumerator());
+		int numerator = denominator / lhs.GetDenominator() * lhs.GetNumerator() + denominator / rhs.GetDenominator() * rhs.GetNumerator();
+		int div = std::gcd(denominator, numerator);
+		return CRational(numerator / div, denominator / div);
 	}
